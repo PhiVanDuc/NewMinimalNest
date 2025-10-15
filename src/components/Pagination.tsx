a3555166ceabda32e27cf6c84e0d39d3706ce5b1
Phi Van Duc <phivanduc325@gmail.com>
2025-10-15 22:29:41 +0700

    Done Products Pagination Part 2

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 import { Input } from "@/components/ui/input";
 
@@ -17,29 +18,54 @@ interface PropsType {
 }
 
 export default function Pagination({ page, totalPage, listRef }: PropsType) {
+    const router = useRouter();
     const [pageValue, setPageValue] = useState(!positiveIntegerValidator(page) ? "1" : page);
+
+    const navigatePage = (value: string) => {
+        const params = new URLSearchParams(window.location.search);
+        params.set("page", value);
+        router.replace(`?${params.toString()}`, { scroll: false });
+
+        setTimeout(() => {
+            if (listRef.current) {
+                const top = listRef.current.offsetTop - 100;
+                window.scrollTo({ top: top > 0 ? top : 0, behavior: "smooth" });
+            }
+        }, 50);
+    }
 
     const handleChangePageValue = (e: React.ChangeEvent<HTMLInputElement>) => {
         const target = e.target;
         let value = target.value;
 
         if (value) {
-            if (!positiveIntegerValidator(value)) value = "1";
-            if (positiveIntegerValidator(value) && Number(value) > Number(totalPage)) value = totalPage;
+            const isValid = positiveIntegerValidator(value);
+
+            if (!isValid) value = "1";
+            else if (Number(value) > Number(totalPage)) value = totalPage;
         }
 
         setPageValue(value);
     }
 
-    const handleNavigate = (direction: "prev" | "next") => {
-        if (listRef.current) {
-            const top = listRef.current.offsetTop - 100;
+    const handleNavigateEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== "Enter") return;
 
-            window.scrollTo({
-                top: top > 0 ? top : 0,
-                behavior: "smooth",
-            });
-        }
+        // Fetch data
+
+        navigatePage(pageValue);
+    }
+
+    const handleNavigate = (direction: "prev" | "next") => {
+        let newPage = Number(pageValue);
+
+        if (direction === "prev" && newPage > 1) newPage -= 1;
+        else if (direction === "next" && newPage < Number(totalPage)) newPage += 1;
+
+        // Fetch data
+
+        setPageValue(`${newPage}`);
+        navigatePage(`${newPage}`);
     }
 
     return (
@@ -57,6 +83,7 @@ export default function Pagination({ page, totalPage, listRef }: PropsType) {
                 <Input
                     value={pageValue}
                     onChange={handleChangePageValue}
+                    onKeyUp={handleNavigateEnter}
                     className="h-[30px] w-[50px] border-2 text-white focus-visible:border-white focus-visible:ring-white/40"
                 />
 
@@ -74,6 +101,7 @@ export default function Pagination({ page, totalPage, listRef }: PropsType) {
                 <button
                     className="shrink-0 flex items-center justify-center w-[40px] aspect-square rounded-full bg-theme-main text-white cursor-pointer"
                     onClick={() => { handleNavigate("prev") }}
+                    disabled={pageValue === "1"}
                 >
                     <FaChevronLeft
                         className={cn(
@@ -86,6 +114,7 @@ export default function Pagination({ page, totalPage, listRef }: PropsType) {
                 <button
                     className="shrink-0 flex items-center justify-center w-[40px] aspect-square rounded-full bg-theme-main text-white cursor-pointer"
                     onClick={() => { handleNavigate("next") }}
+                    disabled={pageValue === totalPage}
                 >
                     <FaChevronRight
                         className={cn(

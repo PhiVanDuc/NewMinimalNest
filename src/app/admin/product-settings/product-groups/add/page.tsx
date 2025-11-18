31d4692c4dad469bfcf7db784570272bb230f863
Phi Duc <phivanduc325@gmail.com>
2025-11-19 00:42:27 +0700

    Useless Change On Laptop

--- src/app/admin/product-settings/product-groups/add/page.tsx
+++ src/app/admin/product-settings/product-groups/add/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 
 import Header from "@/components/Header";
@@ -15,7 +15,8 @@ import {
 } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
-import { FaEye } from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
+import { FaEye, FaPlus } from "react-icons/fa6";
 import { FaEyeSlash } from "react-icons/fa6";
 
 export interface FormValuesType {
@@ -24,12 +25,8 @@ export interface FormValuesType {
 }
 
 export default function Page() {
-    const form = useForm<FormValuesType>({
-        defaultValues: {
-            name: "",
-            products: []
-        }
-    });
+    const filterFormRef = useRef(null);
+    const mainFormRef = useRef(null);
 
     const [isShowProductPicker, setIsShowProductPicker] = useState(true);
     const [productsFiltered, setProductsFiltered] = useState([]);
@@ -37,6 +34,15 @@ export default function Page() {
         name: "",
         categories: []
     });
+
+    const form = useForm<FormValuesType>({
+        defaultValues: {
+            name: "",
+            products: []
+        }
+    });
+
+    const watchProducts = form.watch("products");
 
     const handleSubmit = (data: FormValuesType) => {
         console.log(data);
@@ -55,13 +61,17 @@ export default function Page() {
                     onSubmit={form.handleSubmit(handleSubmit)}
                     className="space-y-[40px]"
                 >
-                    <div className="relative pl-[24px] space-y-[20px]">
+                    <div
+                        ref={filterFormRef}
+                        className="relative pl-[24px] space-y-[20px]"
+                    >
                         <div className="absolute left-0 top-0 bottom-0 w-[4px] h-full rounded-full bg-theme-main" />
-
-
                     </div>
 
-                    <div className="relative pl-[24px] space-y-[20px]">
+                    <div
+                        ref={mainFormRef}
+                        className="relative pl-[24px] space-y-[20px]"
+                    >
                         <div className="absolute left-0 top-0 bottom-0 w-[4px] h-full rounded-full bg-theme-main" />
 
                         <FormField
@@ -83,7 +93,24 @@ export default function Page() {
                                 )
                             }}
                         />
+
+                        {
+                            watchProducts.length === 0 ?
+                                (
+                                    <div className="flex justify-center p-[20px] rounded-[10px] bg-zinc-100">
+                                        <p className="desc-basic">Chưa có sản phẩm nào trong nhóm.</p>
+                                    </div>
+                                ) :
+                                (
+                                    <ul></ul>
+                                )
+                        }
                     </div>
+
+                    <Button className="w-full bg-theme-main hover:bg-theme-main/95">
+                        <FaPlus />
+                        Thêm nhóm sản phẩm
+                    </Button>
                 </form>
             </Form>
 
